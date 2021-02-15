6ca64e90ade89a592d4e8622f7f37a1878dac9c0
Samantha Knoop <sknoop@hotmail.ca>
2021-02-15 20:22:42 +0000

    added code to format text on a webpage

--- express_server.js
+++ express_server.js
@@ -7,12 +7,19 @@ const urlDatabase = {
   "9sn5xK": "http://www.google.com"
 };
 
+//this will show the word "hello" when going to page "/" (home)
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+//create page containing database info, in a string JSON format
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
+});
+
+//creates a page at the path hello, adds some text with one word bold
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n")
 });
 
 app.listen(PORT, () => {
