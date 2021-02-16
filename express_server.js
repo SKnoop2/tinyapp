067a08a96a7fc7dbc5acc3199b8cce9c7d986df8
Samantha Knoop <sknoop@hotmail.ca>
2021-02-16 15:51:59 +0000

    added code to redirect user to short URL page after form submission. Also added the link for the user to click on to take them to their long URL

--- express_server.js
+++ express_server.js
@@ -4,21 +4,15 @@ const PORT = 8080;
 const bodyParser = require('body-parser');
 
 function generateRandomString() {
-  let string = Math.random().toString(36).substring(7);
-console.log("random", r);
-  return string;
+  const str = Math.random().toString(36).substring(7);
+  // console.log("random", r);
+  return str;
 }
-console.log(generateRandomString());
+// console.log(generateRandomString());
 app.use(bodyParser.urlencoded({extended: true}));
 
 //tells express app to use EJS ar its templating engine
 app.set("view engine", "ejs");
-
-//define a route that will match a post request
-app.post("/urls", (req, res) =>{
-  console.log(req.body); //logs to server console
-  res.send("Ok"); //respond with ok (will be changed later)
-});
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -52,10 +46,28 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+app.post("/urls", (req, res) =>{
+  const shortURL = generateRandomString();
+  const longURL = req.body.longURL;
+  urlDatabase[shortURL] = longURL;
+  // responds with a redirection to /urls/:shortURL, where shortURL is the random string we generated
+  // app.get("/urls/:shortURL", callback); //callback is not defined
+  res.redirect(`/urls/${shortURL}`) // Failed to lookup view "/urls/:shortURL" in views directory
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] }
   res.render("urls_show", templateVars);
 })
+
+app.get("/u/:shortURL", (req, res) => {
+  //first find your longURL
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
+//define a route that will match a post request
+// We need to save the longURL and shortURL to our urlDatabase in format {shortUrl:longURL} 
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`)
