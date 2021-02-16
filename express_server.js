cf6a053d3a25f87f1be917634d01de329579c6ab
Samantha Knoop <sknoop@hotmail.ca>
2021-02-16 03:22:09 +0000

    added function to generate a random string (url)

--- express_server.js
+++ express_server.js
@@ -3,6 +3,12 @@ const app = express();
 const PORT = 8080;
 const bodyParser = require('body-parser');
 
+function generateRandomString() {
+  let string = Math.random().toString(36).substring(7);
+console.log("random", r);
+  return string;
+}
+console.log(generateRandomString());
 app.use(bodyParser.urlencoded({extended: true}));
 
 //tells express app to use EJS ar its templating engine
