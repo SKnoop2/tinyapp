fbc8900670bdc9f7e79ecf07bf6c116b05052096
Samantha Knoop <sknoop@hotmail.ca>
2021-02-16 00:11:34 +0000

    learned how to create separate pages for multiple short ULR, without needing to duplicating code for each page

--- express_server.js
+++ express_server.js
@@ -15,14 +15,6 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-//add new route handler. Use res.render to pass the url data to our template (urls_index)
-//express knows to look inside a views directory for template file with extension .ejs, thus we don't need to tell the app where to find the files
-app.get("/urls", (req, res) => {
-  //when we send even one variable, we need to send it inside an object
-  const urlsObject = { urls: urlDatabase };
-  res.render("urls_index", urlsObject)
-})
-
 //create page containing database info, in a string JSON format
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
@@ -30,8 +22,21 @@ app.get("/urls.json", (req, res) => {
 
 //creates a page at the path hello, adds some text with one word bold
 app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n")
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+//add new route handler. Use res.render to pass the url data to our template (urls_index)
+//express knows to look inside a views directory for template file with extension .ejs, thus we don't need to tell the app where to find the files
+app.get("/urls", (req, res) => {
+  //when we send even one variable, we need to send it inside an object
+  const urlsObject = { urls: urlDatabase };
+  res.render("urls_index", urlsObject);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] }
+  res.render("urls_show", templateVars);
+})
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`)
